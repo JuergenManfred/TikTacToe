358e4f8f4226a556e8bd6168c3147a99443d1086
JuergenManfred <everest3221@gmail.com>
2023-12-12 16:33:43 +0100

    add Gameboard and Players

--- script.js
+++ script.js
@@ -1,2 +1,27 @@
-// script.js
-console.log('Hello, world!');
+
+(function Gameboard() {
+const board = []
+const row = 4;
+const column = 4;
+
+
+for (let i = 0; i < row; i++) {
+    board[i] = []
+    for (let j = 0; j < column; j++) {
+
+    }
+}
+
+})();
+
+const Players = [
+    {PlayerOne: 0},
+    {PlayerTwo: 0}
+]
+
+function Cell() {
+    let value = 0;
+    const addToken = (player) => {
+        value = player;
+    }
+}
